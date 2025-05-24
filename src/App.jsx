da8f3136cbbf0135ac98ca7f9546f2735092c2da
Moemen Gaballa <moemengaballa@gmail.com>
2025-05-24 20:22:56 +0300

    completing the static UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import {PageWrapper} from "./components/PageWrapper.jsx";
 import {Container} from "./components/Container.jsx";
 import {Header} from "./components/Header.jsx";
+import {Search} from "./components/Search.jsx";
+import {ShortList} from "./components/ShortList.jsx";
+import {PuppiesList} from "./components/PuppiesList.jsx";
+import {NewPuppyForm} from "./components/NewPuppyForm.jsx";
 
 export function App() {
     return (
         <PageWrapper>
             <Container>
                 <Header/>
-                <main></main>
+                <Main/>
             </Container>
         </PageWrapper>
     );
 }
 
+function Main() {
+    return (<main>
+            <div className="mt-24 grid gap-8 sm:grid-cols-2">
+                <Search/>
+                <ShortList/>
+            </div>
+            <PuppiesList/>
 
+            <NewPuppyForm />
+        </main>
+    );
+}
 
 
 
