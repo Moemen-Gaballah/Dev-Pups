5dbc3feaed777531825e5600060b7cc9838f9b11
Moemen Gaballa <moemengaballa@gmail.com>
2025-05-24 19:41:01 +0300

    init project react, some basic component and update vite.config.js file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {PageWrapper} from "./components/PageWrapper.jsx";
+import {Container} from "./components/Container.jsx";
+import {Header} from "./components/Header.jsx";
+
+export function App() {
+    return (
+        <PageWrapper>
+            <Container>
+                <Header/>
+                <main></main>
+            </Container>
+        </PageWrapper>
+    );
+}
+
+
+
+
+
